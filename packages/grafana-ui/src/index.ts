c7242237a9dc6ad93d637070a57dbbcf62bdcd15
Shailendra Sharma <shailendra.sharma@gmail.com>
2024-09-18 06:26:22 +0530

    added additional exports

--- packages/grafana-ui/src/index.ts
+++ packages/grafana-ui/src/index.ts
@@ -12,3 +12,12 @@ export * from './slate-plugins';
 
 // Moved to `@grafana/schema`, in Grafana 9, this will be removed
 export * from './schema';
+
+import { contextSrv } from '../../../public/app/core/services/context_srv';
+import { getTransformationVars } from '../../../public/app/features/correlations/transformations';
+import { ExploreItemState } from '../../../public/app/types/explore';
+import { getLinkSrv } from '../../../public/app/features/panel/panellinks/link_srv';
+import { getUrlStateFromPaneState } from '../../../public/app/features/explore/hooks/useStateSync';
+
+export { contextSrv, getTransformationVars, getLinkSrv, getUrlStateFromPaneState };
+export type { ExploreItemState };
